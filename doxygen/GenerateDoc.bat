7eb145c9b65db3ddca3fcfc550a0105f4deddb9d
carlos.fernandez <carlos.fdez1310@gmail.com>
2024-12-08 21:31:46 +0100

    Update GenerateDoc.bat to allow execution from other paths

--- doxygen/GenerateDoc.bat
+++ doxygen/GenerateDoc.bat
@@ -1,2 +1,10 @@
-Doxygen Doxyfile.dox >log.txt 2>&1
-..\docs\index.html
+@echo off
+
+set current_dir="%cd%"
+cd /d %~dp0
+
+echo Generating doxygen documentation...
+Doxygen %~dp0Doxyfile.dox >%~dp0log.txt 2>&1
+%~dp0..\docs\index.html
+
+cd /d %current_dir%
